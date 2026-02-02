9df1c01bca815b0fe4b21822c0a1fc7586b22b2f
Shamima <shamima2802@gmail.com>
2026-02-02 19:40:27 +0530

    app fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// 1. HashRouter-ah import pannunga
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"; 
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 
@@ -8,20 +9,21 @@ function App() {
   const isAdmin = token && role === 'ADMIN';
 
   return (
-    <BrowserRouter>
+   
+    <HashRouter>
       <Routes>
         <Route path="/" element={<AdminLogin />} />
         <Route path="/login" element={<AdminLogin />} />
         
-       
         <Route 
           path="/dashboard" 
           element={isAdmin ? <AdminDashboard /> : <Navigate to="/" />} 
         />
         
+        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
